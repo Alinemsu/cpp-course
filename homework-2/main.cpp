9a19e931cfd386e32d919cdc51778c81394ddff6
Alina Yusufova <153069148+Alinemsu@users.noreply.github.com>
2026-02-16 16:37:33 +0500

    Add files via upload

---
+++ homework-2/main.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include "matrix.hpp"
+
+int main()
+{
+    Matrix<int, 0> matrix;
+
+    for (int i = 0; i < 10; ++i)
+        matrix[i][i] = i;
+
+    for (int i = 0; i < 10; ++i) {
+        matrix[i][9 - i] = 9 - i;
+    }
+
+    std::cout << "Fragment [1,1]..[8,8]: " << std::endl;
+    for (int i = 1; i < 9; ++i) {
+        for (int j = 1; j < 9; ++j) {
+            std::cout << (int)matrix[i][j];
+            if (j != 8)
+                std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "cells with value: " << matrix.size() << std::endl;
+
+    std::cout << "the positions of cells with value: " << matrix.size() << std::endl;
+    for (auto c : matrix) {
+        int i, j, v;
+        std::tie(i, j, v) = c;
+        std::cout << i << " " << j << " " << v << std::endl;
+    }
+
+    {
+        Matrix<int, 0, 2> matrix2;
+
+        ((matrix2[100][100] = 314) = 0) = 217;
+
+        std::cout << (int)matrix2[100][100] << std::endl;
+        std::cout << matrix2.size() << std::endl;
+    }
+
+    Matrix<int, 0, 3> cube;
+
+    for (int i = 0; i < 10; ++i) {
+        cube[i][i][i] = i + 1;
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        cube[i][i][9 - i] = 100 + i;
+    }
+
+    std::cout << "3D checks:" << std::endl;
+    std::cout << "(0,0,0) = " << (int)cube[0][0][0] << std::endl;
+    std::cout << "(0,0,9) = " << (int)cube[0][0][9] << std::endl;
+    std::cout << "(1,2,3) = " << (int)cube[1][2][3] << std::endl;
+
+    std::cout << "3D cells with value: " << cube.size() << std::endl;
+
+    for (auto c : cube) {
+        Matrix<int, 0, 3>::Key key;
+        int value;
+        std::tie(key, value) = c;
+
+        std::cout << key[0] << " " << key[1] << " " << key[2]
+            << " " << value << std::endl;
+    }
+
+    {
+        Matrix<int, 0, 3> cube2;
+        ((cube2[1][2][3] = 314) = 0) = 217;
+        std::cout << "3D chain value: " << (int)cube2[1][2][3] << std::endl;
+        std::cout << "3D chain size: " << cube2.size() << std::endl;
+    }
+    return 0;
+}
---
+++ homework-2/matrix.hpp
@@ -0,0 +1,249 @@
+#pragma once
+#include <map>
+#include <array>
+#include <tuple>
+
+template<class ValueType, ValueType DefaultValue, std::size_t DimensionCount = 2>
+class Matrix;
+
+template<class ValueType, ValueType DefaultValue, std::size_t DimensionCount, std::size_t FixedCount>
+class MatrixRow;
+
+template<class ValueType, ValueType DefaultValue, std::size_t DimensionCount>
+class Matrix {
+public:
+    Matrix() = default;
+
+    int size() const {
+        return (int)data_.size();
+    }
+
+    struct Key {
+        std::array<int, DimensionCount> index;
+
+        int& operator[](std::size_t k) {
+            return index[k];
+        }
+
+        const int& operator[](std::size_t k) const {
+            return index[k];
+        }
+    };
+
+    struct KeyLess {
+        bool operator()(const Key& left, const Key& right) const {
+            for (std::size_t k = 0; k < DimensionCount; ++k) {
+                if (left[k] != right[k]) {
+                    return left[k] < right[k];
+                }
+            }
+            return false;
+        }
+    };
+
+private:
+    std::map<Key, ValueType, KeyLess> data_;
+
+    ValueType get_(const Key& key) const {
+        auto it = data_.find(key);
+        if (it == data_.end()) {
+            return DefaultValue;
+        }
+        return it->second;
+    }
+
+    void set_(const Key& key, const ValueType& value) {
+        if (value == DefaultValue) {
+            data_.erase(key);
+        }
+        else {
+            data_[key] = value;
+        }
+    }
+
+    template<class FriendValueType, FriendValueType FriendDefaultValue, std::size_t FriendDimensionCount, std::size_t FriendFixedCount>
+    friend class MatrixRow;
+
+public:
+    MatrixRow<ValueType, DefaultValue, DimensionCount, 1> operator[](int firstIndex) {
+        Key key{};
+        key[0] = firstIndex;
+        return MatrixRow<ValueType, DefaultValue, DimensionCount, 1>(this, key);
+    }
+
+    class iterator {
+        typename std::map<Key, ValueType, KeyLess>::iterator it_;
+    public:
+        iterator(typename std::map<Key, ValueType, KeyLess>::iterator it)
+            : it_(it) {
+        }
+
+        iterator& operator++() {
+            ++it_;
+            return *this;
+        }
+
+        bool operator!=(const iterator& other) const {
+            return it_ != other.it_;
+        }
+
+        std::tuple<Key, ValueType> operator*() const {
+            return std::tuple<Key, ValueType>(it_->first, it_->second);
+        }
+    };
+
+    iterator begin() {
+        return iterator(data_.begin());
+    }
+
+    iterator end() {
+        return iterator(data_.end());
+    }
+};
+
+template<class ValueType, ValueType DefaultValue, std::size_t DimensionCount, std::size_t FixedCount>
+class MatrixRow {
+    Matrix<ValueType, DefaultValue, DimensionCount>* matrix_;
+    typename Matrix<ValueType, DefaultValue, DimensionCount>::Key key_;
+public:
+    MatrixRow(Matrix<ValueType, DefaultValue, DimensionCount>* matrix,
+        const typename Matrix<ValueType, DefaultValue, DimensionCount>::Key& key)
+        : matrix_(matrix), key_(key) {
+    }
+
+    MatrixRow<ValueType, DefaultValue, DimensionCount, FixedCount + 1> operator[](int nextIndex) {
+        auto nextKey = key_;
+        nextKey[FixedCount] = nextIndex;
+        return MatrixRow<ValueType, DefaultValue, DimensionCount, FixedCount + 1>(matrix_, nextKey);
+    }
+};
+
+template<class ValueType, ValueType DefaultValue, std::size_t DimensionCount>
+class MatrixRow<ValueType, DefaultValue, DimensionCount, DimensionCount> {
+    Matrix<ValueType, DefaultValue, DimensionCount>* matrix_;
+    typename Matrix<ValueType, DefaultValue, DimensionCount>::Key key_;
+public:
+    MatrixRow(Matrix<ValueType, DefaultValue, DimensionCount>* matrix,
+        const typename Matrix<ValueType, DefaultValue, DimensionCount>::Key& key)
+        : matrix_(matrix), key_(key) {
+    }
+
+    operator ValueType() const {
+        return matrix_->get_(key_);
+    }
+
+    MatrixRow& operator=(const ValueType& value) {
+        matrix_->set_(key_, value);
+        return *this;
+    }
+};
+
+template<class ValueType, ValueType DefaultValue>
+class Matrix<ValueType, DefaultValue, 2> {
+public:
+    Matrix() = default;
+
+    int size() const {
+        return (int)data_.size();
+    }
+
+    struct Key {
+        int row;
+        int col;
+    };
+
+    struct KeyLess {
+        bool operator()(const Key& a, const Key& b) const {
+            if (a.row != b.row) {
+                return a.row < b.row;
+            }
+            return a.col < b.col;
+        }
+    };
+
+private:
+    std::map<Key, ValueType, KeyLess> data_;
+
+    ValueType get_(int row, int col) const {
+        auto it = data_.find(Key{ row, col });
+        if (it == data_.end()) {
+            return DefaultValue;
+        }
+        return it->second;
+    }
+
+    void set_(int row, int col, const ValueType& value) {
+        if (value == DefaultValue) {
+            data_.erase(Key{ row, col });
+        }
+        else {
+            data_[Key{ row, col }] = value;
+        }
+    }
+
+public:
+    class Cell {
+        Matrix* matrix_;
+        int row_;
+        int col_;
+    public:
+        Cell(Matrix* m, int row, int col)
+            : matrix_(m), row_(row), col_(col) {
+        }
+
+        operator ValueType() const {
+            return matrix_->get_(row_, col_);
+        }
+
+        Cell& operator=(const ValueType& value) {
+            matrix_->set_(row_, col_, value);
+            return *this;
+        }
+    };
+
+    class Row {
+        Matrix* matrix_;
+        int row_;
+    public:
+        Row(Matrix* m, int row)
+            : matrix_(m), row_(row) {
+        }
+
+        Cell operator[](int col) {
+            return Cell(matrix_, row_, col);
+        }
+    };
+
+    Row operator[](int row) {
+        return Row(this, row);
+    }
+
+    class iterator {
+        typename std::map<Key, ValueType, KeyLess>::iterator it_;
+    public:
+        iterator(typename std::map<Key, ValueType, KeyLess>::iterator it)
+            : it_(it) {
+        }
+
+        iterator& operator++() {
+            ++it_;
+            return *this;
+        }
+
+        bool operator!=(const iterator& other) const {
+            return it_ != other.it_;
+        }
+
+        std::tuple<int, int, ValueType> operator*() const {
+            return std::tuple<int, int, ValueType>(it_->first.row, it_->first.col, it_->second);
+        }
+    };
+
+    iterator begin() {
+        return iterator(data_.begin());
+    }
+
+    iterator end() {
+        return iterator(data_.end());
+    }
+};
