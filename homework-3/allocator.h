f158010d2ef96e569488951d00848d5cac9484f2
Alina Yusufova <153069148+Alinemsu@users.noreply.github.com>
2026-02-20 20:54:32 +0500

    Update allocator.h

--- homework-3/allocator.h
+++ homework-3/allocator.h
@@ -3,27 +3,88 @@
 
 typedef unsigned long long size_type;
 
-template<class T, size_type limit>
+inline size_type align_up_alloc(size_type x, size_type a)
+{
+	if (a <= 1)
+		return x;
+
+	while (x % a != 0)
+		x = x + 1;
+
+	return x;
+}
+
+struct BufferState
+{
+	unsigned char* data;
+	size_type capacity_bytes;
+	size_type used_bytes;
+	unsigned long refs;
+
+	BufferState(size_type bytes)
+	{
+		data = 0;
+		capacity_bytes = bytes;
+		used_bytes = 0;
+		refs = 1;
+
+		if (capacity_bytes != 0)
+			data = (unsigned char*)::operator new((size_t)capacity_bytes);
+	}
+
+	~BufferState()
+	{
+		::operator delete((void*)data);
+	}
+
+	BufferState(const BufferState&) = delete;
+	BufferState& operator=(const BufferState&) = delete;
+};
+
+template<class T>
 class Allocator
 {
 public:
 	typedef T value_type;
 
-	Allocator() {}
+	Allocator(size_type capacity_bytes = 0)
+	{
+		state_ = new BufferState(capacity_bytes);
+	}
+
+	Allocator(const Allocator& other)
+	{
+		state_ = other.state_;
+		state_->refs = state_->refs + 1;
+	}
 
 	template<class U>
-	Allocator(const Allocator<U, limit>&) {}
+	Allocator(const Allocator<U>& other)
+	{
+		state_ = other.state_;
+		state_->refs = state_->refs + 1;
+	}
+
+	~Allocator()
+	{
+		state_->refs = state_->refs - 1;
+		if (state_->refs == 0)
+			delete state_;
+	}
 
 	T* allocate(size_type n)
 	{
 		if (n == 0)
 			return 0;
 
-		if (used() + n > limit + 1)
+		size_type bytes = n * (size_type)sizeof(T);
+		size_type start = align_up_alloc(state_->used_bytes, (size_type)alignof(T));
+
+		if (start + bytes > state_->capacity_bytes)
 			throw std::bad_alloc();
 
-		T* p = data() + used();
-		used() = used() + n;
+		T* p = (T*)(state_->data + start);
+		state_->used_bytes = start + bytes;
 
 		return p;
 	}
@@ -31,36 +92,20 @@ public:
 	void deallocate(T*, size_type) {}
 
 	template<class U>
-	struct rebind
+	bool operator==(const Allocator<U>& other) const
 	{
-		typedef Allocator<U, limit> other;
-	};
+		return state_ == other.state_;
+	}
+
+	template<class U>
+	bool operator!=(const Allocator<U>& other) const
+	{
+		return state_ != other.state_;
+	}
 
 private:
-	struct Storage
-	{
-		T* ptr;
+	template<class U>
+	friend class Allocator;
 
-		Storage()
-		{
-			ptr = (T*)::operator new((limit + 1) * (size_type)sizeof(T));
-		}
-
-		~Storage()
-		{
-			::operator delete(ptr);
-		}
-	};
-
-	static T* data()
-	{
-		static Storage s;
-		return s.ptr;
-	}
-
-	static size_type& used()
-	{
-		static size_type u = 0;
-		return u;
-	}
+	BufferState* state_;
 };
