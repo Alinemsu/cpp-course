a67cec7393e7c7f679e9d7602efd67166e1f997f
Alina Yusufova <153069148+Alinemsu@users.noreply.github.com>
2026-02-21 00:06:32 +0500

    upd

--- homework-3/container.hpp
+++ homework-3/container.hpp
@@ -8,106 +8,106 @@ template<class T, class Alloc = std::allocator<T>>
 class Container
 {
 private:
-    struct Node
-    {
-        T value;
-        Node* next;
-    };
+	struct Node
+	{
+		T value;
+		Node* next;
+	};
 
-    typedef typename std::allocator_traits<Alloc>::template rebind_alloc<Node> NodeAlloc;
+	typedef typename std::allocator_traits<Alloc>::template rebind_alloc<Node> NodeAlloc;
 
 public:
-    class iterator
-    {
-    public:
-        iterator(Node* p)
-        {
-            p_ = p;
-        }
+	class iterator
+	{
+	public:
+		iterator(Node* p)
+		{
+			p_ = p;
+		}
 
-        T& operator*()
-        {
-            return p_->value;
-        }
+		T& operator*()
+		{
+			return p_->value;
+		}
 
-        iterator& operator++()
-        {
-            p_ = p_->next;
-            return *this;
-        }
+		iterator& operator++()
+		{
+			p_ = p_->next;
+			return *this;
+		}
 
-        bool operator!=(const iterator& other)
-        {
-            return p_ != other.p_;
-        }
+		bool operator!=(const iterator& other)
+		{
+			return p_ != other.p_;
+		}
 
-    private:
-        Node* p_;
-    };
+	private:
+		Node* p_;
+	};
 
-    Container(const Alloc& a = Alloc())
-        : alloc_(a)
-    {
-        head_ = 0;
-        tail_ = 0;
-        size_ = 0;
-    }
+	Container(const Alloc& a = Alloc())
+		: alloc_(a)
+	{
+		head_ = 0;
+		tail_ = 0;
+		size_ = 0;
+	}
 
-    ~Container()
-    {
-        Node* p = head_;
+	~Container()
+	{
+		Node* current = head_;
 
-        while (p != 0)
-        {
-            Node* next = p->next;
+		while (current != 0)
+		{
+			Node* next = current->next;
 
-            std::allocator_traits<NodeAlloc>::destroy(alloc_, p);
-            std::allocator_traits<NodeAlloc>::deallocate(alloc_, p, 1);
+			std::allocator_traits<NodeAlloc>::destroy(alloc_, current);
+			std::allocator_traits<NodeAlloc>::deallocate(alloc_, current, 1);
 
-            p = next;
-        }
-    }
+			current = next;
+		}
+	}
 
-    Container(const Container&) = delete;
-    Container& operator=(const Container&) = delete;
+	Container(const Container&) = delete;
+	Container& operator=(const Container&) = delete;
 
-    void push_back(const T& x)
-    {
-        Node* n = std::allocator_traits<NodeAlloc>::allocate(alloc_, 1);
+	void push_back(const T& x)
+	{
+		Node* n = std::allocator_traits<NodeAlloc>::allocate(alloc_, 1);
 
-        std::allocator_traits<NodeAlloc>::construct(alloc_, n);
+		std::allocator_traits<NodeAlloc>::construct(alloc_, n);
 
-        n->value = x;
-        n->next = 0;
+		n->value = x;
+		n->next = 0;
 
-        if (tail_ == 0)
-            head_ = n;
+		if (tail_ == 0)
+			head_ = n;
 
-        if (tail_ != 0)
-            tail_->next = n;
+		if (tail_ != 0)
+			tail_->next = n;
 
-        tail_ = n;
-        size_ = size_ + 1;
-    }
+		tail_ = n;
+		size_ = size_ + 1;
+	}
 
-    iterator begin()
-    {
-        return iterator(head_);
-    }
+	iterator begin()
+	{
+		return iterator(head_);
+	}
 
-    iterator end()
-    {
-        return iterator(0);
-    }
+	iterator end()
+	{
+		return iterator(0);
+	}
 
-    size_type size() const
-    {
-        return size_;
-    }
+	size_type size() const
+	{
+		return size_;
+	}
 
 private:
-    NodeAlloc alloc_;
-    Node* head_;
-    Node* tail_;
-    size_type size_;
+	NodeAlloc alloc_;
+	Node* head_;
+	Node* tail_;
+	size_type size_;
 };
