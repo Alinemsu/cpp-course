7e831845167223b90cbf7a6cbd4c09d5c89aecdf
Alina Yusufova <153069148+Alinemsu@users.noreply.github.com>
2026-02-21 00:00:25 +0500

    Update allocator.hpp

--- homework-3/allocator.hpp
+++ homework-3/allocator.hpp
@@ -1,111 +1,113 @@
 #pragma once
-#include <new>
+#include <cstddef>  // std::size_t
+#include <cstdlib>  // std::malloc, std::free
+#include <new>      // std::bad_alloc
 
-typedef unsigned long long size_type;
-
-inline size_type align_up_alloc(size_type x, size_type a)
+inline std::size_t align_up(std::size_t x, std::size_t a)
 {
-	if (a <= 1)
-		return x;
+    if (a <= 1) return x;
 
-	while (x % a != 0)
-		x = x + 1;
+    while (x % a != 0)
+        x = x + 1;
 
-	return x;
+    return x;
 }
 
 struct BufferState
 {
-	unsigned char* data;
-	size_type capacity_bytes;
-	size_type used_bytes;
-	unsigned long refs;
+    unsigned char* data;
+    std::size_t capacity_bytes;
+    std::size_t used_bytes;
+    unsigned long refs;
 
-	BufferState(size_type bytes)
-	{
-		data = 0;
-		capacity_bytes = bytes;
-		used_bytes = 0;
-		refs = 1;
+    BufferState(std::size_t bytes)
+    {
+        data = 0;
+        capacity_bytes = bytes;
+        used_bytes = 0;
+        refs = 1;
 
-		if (capacity_bytes != 0)
-			data = (unsigned char*)::operator new((size_t)capacity_bytes);
-	}
+        if (capacity_bytes != 0)
+        {
+            data = (unsigned char*)std::malloc(capacity_bytes);
+            if (data == 0)
+                throw std::bad_alloc();
+        }
+    }
 
-	~BufferState()
-	{
-		::operator delete((void*)data);
-	}
+    ~BufferState()
+    {
+        std::free(data);
+    }
 
-	BufferState(const BufferState&) = delete;
-	BufferState& operator=(const BufferState&) = delete;
+    BufferState(const BufferState&) = delete;
+    BufferState& operator=(const BufferState&) = delete;
 };
 
 template<class T>
 class Allocator
 {
 public:
-	typedef T value_type;
+    typedef T value_type;
 
-	Allocator(size_type capacity_bytes = 0)
-	{
-		state_ = new BufferState(capacity_bytes);
-	}
+    Allocator(std::size_t capacity_bytes = 0)
+    {
+        state_ = new BufferState(capacity_bytes);
+    }
 
-	Allocator(const Allocator& other)
-	{
-		state_ = other.state_;
-		state_->refs = state_->refs + 1;
-	}
+    Allocator(const Allocator& other)
+    {
+        state_ = other.state_;
+        state_->refs = state_->refs + 1;
+    }
 
-	template<class U>
-	Allocator(const Allocator<U>& other)
-	{
-		state_ = other.state_;
-		state_->refs = state_->refs + 1;
-	}
+    template<class U>
+    Allocator(const Allocator<U>& other)
+    {
+        state_ = other.state_;
+        state_->refs = state_->refs + 1;
+    }
 
-	~Allocator()
-	{
-		state_->refs = state_->refs - 1;
-		if (state_->refs == 0)
-			delete state_;
-	}
+    ~Allocator()
+    {
+        state_->refs = state_->refs - 1;
+        if (state_->refs == 0)
+            delete state_;
+    }
 
-	T* allocate(size_type n)
-	{
-		if (n == 0)
-			return 0;
+    T* allocate(std::size_t n)
+    {
+        if (n == 0)
+            return 0;
 
-		size_type bytes = n * (size_type)sizeof(T);
-		size_type start = align_up_alloc(state_->used_bytes, (size_type)alignof(T));
+        std::size_t bytes = n * sizeof(T);
+        std::size_t start = align_up(state_->used_bytes, alignof(T));
 
-		if (start + bytes > state_->capacity_bytes)
-			throw std::bad_alloc();
+        if (start + bytes > state_->capacity_bytes)
+            throw std::bad_alloc();
 
-		T* p = (T*)(state_->data + start);
-		state_->used_bytes = start + bytes;
+        T* p = (T*)(state_->data + start);
+        state_->used_bytes = start + bytes;
+        return p;
+    }
 
-		return p;
-	}
+    void deallocate(T*, std::size_t) {}
 
-	void deallocate(T*, size_type) {}
+    template<class U>
+    bool operator==(const Allocator<U>& other) const
+    {
+        return state_ == other.state_;
+    }
 
-	template<class U>
-	bool operator==(const Allocator<U>& other) const
-	{
-		return state_ == other.state_;
-	}
-
-	template<class U>
-	bool operator!=(const Allocator<U>& other) const
-	{
-		return state_ != other.state_;
-	}
+    template<class U>
+    bool operator!=(const Allocator<U>& other) const
+    {
+        return state_ != other.state_;
+    }
 
 private:
-	template<class U>
-	friend class Allocator;
+    template<class U>
+    friend class Allocator;
 
-	BufferState* state_;
+    BufferState* state_;
 };
